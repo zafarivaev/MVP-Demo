4f5c0a46c932f4ea31003c476f491ff7af2f2718
zafarivaev <z.ivaev@mail.ru>
2020-01-28 21:28:45 +0500

    Initial Commit

---
+++ MVP-Realm/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MVP-Realm
+//
+//  Created by Zafar on 1/28/20.
+//  Copyright © 2020 Zafar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
